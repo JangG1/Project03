ba0035b0b78e4acfe612a6c22ddf45a25aee4696
JangG1 <jangjiwon0417@naver.com>
2022-10-07 22:39:37 +0900

    Project

---
+++ FastripBackend/src/main/java/com/FT/app/FastripBackendApplication.java
@@ -0,0 +1,13 @@
+package com.FT.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastripBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastripBackendApplication.class, args);
+	}
+
+}

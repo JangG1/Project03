44d4590b496269c503dbc4aa5451cf5886f60c64
JangG1 <jangjiwon0417@naver.com>
2022-10-14 23:56:54 +0900

    Project

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -18,9 +18,9 @@ const routes = [
     component: () => import('../views/Calendar.vue')
   },
   {
-    path: '/WebView',
-    name: 'WebView',
-    component: () => import('../views/WebView.vue')
+    path: '/Tes1t',
+    name: 'Test',
+    component: () => import('../views/Test.vue')
   }
   ,
   {

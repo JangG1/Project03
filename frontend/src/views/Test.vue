dc4fab91647812ed0522db8ded1b7f62fc648776
JangG1 <jangjiwon0417@naver.com>
2022-11-22 20:50:03 +0900

    Project

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -9,47 +9,60 @@
 <script>
 export default {
     name: 'HelloWorld',
-    data() {
-    },
+    data() {},
     methods: {
         naverLoginBtn() {
-
             var client_id = 'z_xevkfqoAuqghG2b8CF';
             var callbackUrl = 'http://localhost:8081/Test'; //서버 주소
             var url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + callbackUrl + '&state=1234';
             window.location.replace(url);
+        },
+        methodA() {
 
-        }
+        },
+        methodB() {
+            var self = this;
+
+            callBack({
+                success: async function (response) {
+                    self.methodA(response)
+
+                }
+
+            })
+
+        },
+
     },
     mounted() {
-    var self = this;
-    try{
-      //네이버로 로그인할때만 실행
-      if(this.$route.query.code.length !== undefined){
-        var callbackFuc = async () => {
+        var self = this;
+        try {
+            //네이버로 로그인할때만 실행
+            if (this.$route.query.code.length !== undefined) {
+                var callbackFuc = async () => {
 
-          const res = await fetch('https://locolhost/api/sns_login_naver', {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              code: `${self.$route.query.code}`,
-              state: `${self.$route.query.state}`,
+                    const res = await fetch('https://locolhost/api/sns_login_naver', {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            code: `${self.$route.query.code}`,
+                            state: `${self.$route.query.state}`,
 
-            }),
-          })
-          const data = await res.json();
-          console.log(`네이버 로그인 : ${data.email}`)
-          
-          //네이버 로그인 인증 코드 (nodejs api)
+                        }),
+                    })
+                    const data = await res.json();
+                    console.log(`네이버 로그인 : ${data.email}`)
 
+                    //네이버 로그인 인증 코드 (nodejs api)
+
+                }
+                callbackFuc();
+            }
+        } catch (e) {
+            console.log(e)
         }
-        callbackFuc();
-      }
-    }catch (e){
-      console.log(e)
     }
- }
 }
 </script>

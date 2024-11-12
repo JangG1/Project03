2d966612c955e5373716d714488a9baaef9f7c0c
JangG1 <jangjiwon0417@naver.com>
2024-11-12 18:46:26 +0900

    CORS UPDATE

--- FastripBackend/src/main/java/com/FT/app/security/config/SecurityConfig.java
+++ FastripBackend/src/main/java/com/FT/app/security/config/SecurityConfig.java
@@ -57,11 +57,10 @@ public class SecurityConfig {
     public CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         //configuration.setAllowedOriginPatterns(Arrays.asList("http://fastrip.shop", exIP, exIP+":9200", exIP+":8200"));
-        //configuration.setAllowedOrigins(Arrays.asList("http://fastrip.shop", exIP, exIP + ":9200", exIP + ":8200"));
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://www.fastrip.shop:9200")); // 명시적으로 프론트 도메인 설정
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
-        //configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(true); // 인증 관련 요청 허용
         configuration.addExposedHeader("Authorization");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
